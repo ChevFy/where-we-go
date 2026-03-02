d3e43cf9f583e02b6e43ca9bcc1726afeb881f3a
Nanachi <arnonjantavara@gmail.com>
2026-03-02 17:51:16 +0700

    WIP: GroupChat frontend + DTO

---
+++ DTO/MessageDto.cs
@@ -0,0 +1,6 @@
+public class ChatMessageDto
+{
+    public required string UserName { get; set; }
+    public required string Message { get; set; }
+    public required string SentAt { get; set; }
+}

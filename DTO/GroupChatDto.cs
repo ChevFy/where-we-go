5e6025ea168670505e6fe37d3e31707b38cd50e6
koonchevychpai123@gmail.com <koonchevychpai123@gmail.com>
2026-03-12 00:45:21 +0700

    add chat

--- DTO/GroupChatDto.cs
+++ DTO/GroupChatDto.cs
@@ -7,3 +7,14 @@ public class GroupChatViewDto
     public string current_user_id { get; set; } = "";
     public List<MessageDto> messages { get; set; } = [];
 }
+
+public class GroupChatListItemDto
+{
+    public Guid group_chat_id { get; set; }
+    public string? name { get; set; }
+
+    public Guid post_id { get; set; }
+    public string? post_title { get; set; }
+
+    public bool is_owner { get; set; }
+}
